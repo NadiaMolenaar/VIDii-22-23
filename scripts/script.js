a7deb29f01b7c3563ac855dbe9d591edb5842d22
NadiaMolenaar <58073536+NadiaMolenaar@users.noreply.github.com>
2023-04-24 09:49:16 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,31 @@
 // JavaScript Document
 console.log("Howdy!");
 
+var playButton = document.getElementsByClassName('play');
+var myVideo = document.getElementsByClassName('video');
+
+// Selecteert de 'play button' en voegt een event listener toe die de video
+// met de classname "video" afspeelt wanneer erop wordt geklikt (if). Als er
+// niet op wordt geklikt, wordt de video gepauzeerd (else).
+playButton.addEventListener('click', function() {
+  if (myVideo.paused) {
+    myVideo.play();
+    // playButton.textContent = "Pause Video";
+  } else {
+    myVideo.pause();
+    // playButton.textContent = "Play Video";
+  }
+});
+
+// Selecteert de 'video' en voegt een event listener toe die de video
+// afspeelt wanneer erop wordt geklikt (if). Als er niet op wordt geklikt,
+// blijft de video gepauzeerd (else).
+myVideo.addEventListener('click', function() {
+  if (myVideo.paused) {
+    myVideo.play();
+    // playButton.textContent = "Pause Video";
+  } else {
+    myVideo.pause();
+    // playButton.textContent = "Play Video";
+  }
+});
